f8a227c081cedf64609b218ead55f5d05a33d329
maxsys <maxsys@189.cn>
2022-09-18 18:52:23 +0800

    welcome.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Loading Tec. 欢迎来到锣鼎科技！"/>
 </template>
 
 <script>
